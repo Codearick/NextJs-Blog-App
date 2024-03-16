fafcbb88f9c8b42464d1c9b2e4f2d6905a8bf956
Codearick <rohan.gautam650@gmail.com>
2024-03-17 00:49:09 +0530

    update

--- app/page.js
+++ app/page.js
@@ -1,13 +1,40 @@
 "use client"
+// import Config from "@/app/config/config";
+import { useEffect,useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import {login, logout} from './store/slices/authSlice'
+import Header from '@/components/Header/page'
+import Footer from '@/components/Footer/Footer'
 
-import Config from "@/app/config/config";
 
 export default function Home() {
-  console.log(Config.appwriteUrl);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      } else{
+        dispatch(logout())
+      }
+    })
+    .catch(error =>{console.log(error.messsage)})
+    .finally(() => setLoading(false))
+  }, [])
   
-  return (
-    <>
-      <h1>A blog app with appwrite</h1>
-    </>
-  );
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+      <Header/>
+        <main>
+         TODO: 
+        </main>
+      <Footer />
+      </div>
+    </div>
+  ) : null
 }
+
