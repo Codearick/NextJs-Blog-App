8e8c88c011fbd4649c13dbc6285c3d791272cb3c
Codearick <rohan.gautam650@gmail.com>
2024-03-18 04:27:32 +0530

    update

--- app/login/page.js
+++ app/login/page.js
@@ -39,12 +39,12 @@ const page = () => {
     return (
         <div className='flex items-center justify-center w-full'>
             <div className='mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10'>
-                <div className="logo mb-2 flex justify-center">
-                    <span className="inline-block w-full max-w-[100px]">
-                        <Logo width="100%" />
+                <div className="logo mb-2 flex items-center justify-center">
+                    <span className="flex items-center justify-center w-full">
+                        <Logo/>
                     </span>
                 </div>
-                <h2 className="text-center text-2xl font-bold leading-tight">Sign in to your account</h2>
+                <h2 className="text-slate-600 text-center text-2xl font-bold leading-tight">Sign in to your account</h2>
                 <p className="mt-2 text-center text-base text-black/60">
                     Don&apos;t have any account?&nbsp;
                     <Link
@@ -56,7 +56,7 @@ const page = () => {
                 </p>
                 {error && <p className='text-red-600 mt-8 text-center'>{error}</p>}
                 <form onSubmit={handleSubmit(onSubmit)} className='mt-8' method='POST'>
-                    <div className='space-y-5'>
+                    <div className='space-y-5 text-slate-600'>
                         <Input
                             label="Email"
                             placeholder="Enter your email"
