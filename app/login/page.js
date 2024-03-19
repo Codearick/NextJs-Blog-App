784e1c22913b8acfbf7597cd0730560c0414053b
Codearick <rohan.gautam650@gmail.com>
2024-03-20 03:12:24 +0530

    update

--- app/login/page.js
+++ app/login/page.js
@@ -1,101 +1,13 @@
 "use client"
-import React, { useState } from 'react'
-import Link from 'next/link'
-import { useRouter } from 'next/navigation'
-import authService from '@/app/appwrite/auth'
-import { Logo, Input, Button } from '@/components/index'
-import { login as authLogin } from '@/app/store/slices/authSlice'
-import { useDispatch } from 'react-redux'
-import { useForm } from 'react-hook-form'
+import React from 'react'
+import {Login as LoginComponent} from '@/components/index'
 
 const page = () => {
-    const dispatch = useDispatch();
-    const router = useRouter();
-    const {
-        register,
-        handleSubmit,
-        watch,
-        formState: { isSubmitting },
-    } = useForm()
-    const [error, setError] = useState("");
-
-    const onSubmit = async (data) => {
-        setError(" ");
-        try {
-            const session = authService.Signup(data);
-            if (session) {
-                const userData = await authService.getCurrentUser();
-                if (userData) {
-                    router.push("/");
-                    dispatch(authLogin(userData));
-                }
-            }
-        } catch (error) {
-            console.log("Error from server: ", error.message)
-            setError(" ")
-        }
-    }
-
-    return (
-        <div className='flex items-center justify-center w-full'>
-            <div className='mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10'>
-                <div className="logo mb-2 flex items-center justify-center">
-                    <span className="flex items-center justify-center w-full">
-                        <Logo/>
-                    </span>
-                </div>
-                <h2 className="text-slate-600 text-center text-2xl font-bold leading-tight">Sign in to your account</h2>
-                <p className="mt-2 text-center text-base text-black/60">
-                    Don&apos;t have any account?&nbsp;
-                    <Link
-                        href="/signup"
-                        className="font-medium text-primary transition-all duration-200 hover:underline"
-                    >
-                        Sign Up
-                    </Link>
-                </p>
-                {error && <p className='text-red-600 mt-8 text-center'>{error}</p>}
-                <form onSubmit={handleSubmit(onSubmit)} className='mt-8' method='POST'>
-                    <div className='space-y-5 text-slate-600'>
-                        <Input
-                            label="Email"
-                            placeholder="Enter your email"
-                            type="email"
-                            {
-                            ...register("email", {
-                                required: true,
-                                validate: {
-                                    matchPattern: (value) => {
-                                        /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ||
-                                            "Email address must be a valid address"
-                                    }
-                                }
-                            })}
-                             />
-                        <Input
-                            label = "Password:"
-                            placeholder = "Enter your password"
-                            type = "password"
-                            {
-                                ...register("password",{
-                                    required : true,
-                                    validate: {
-                                        matchPattern: /^(?=.*[A-Z]).{7,}$/,
-                                        message: "Password length must be 7"
-                                    }
-                                })
-                            }
-                        />
-                        <Button
-                        disabled={isSubmitting}
-                        type="submit"
-                        className="w-full hover:bg-blue-500"
-                        >Sign in</Button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    )
+  return (
+    <div className='py-8'>
+      <LoginComponent/>
+    </div>
+  )
 }
 
 export default page
