aacf150fecdea7310e9d55238e0d6e935f6a0905
Codearick <rohan.gautam650@gmail.com>
2024-03-20 03:14:40 +0530

    update

--- app/store/store.js
+++ app/store/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "./slices/authSlice"
+import authReducer from "./slices/authSlice";
+import postReducer from './slices/postSlice'
 
 const store = configureStore({
     reducer: {
