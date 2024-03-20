72a9a5beb42023e2eb40fb13662c0d6a8b049898
Codearick <rohan.gautam650@gmail.com>
2024-03-21 03:05:08 +0530

    update

--- app/appwrite/appwriteConfig.js
+++ app/appwrite/appwriteConfig.js
@@ -65,12 +65,11 @@ class Service {
 
     async getPost(slug){
         try {
-            await this.databases.getDocument(
+            return await this.databases.getDocument(
                 Config.appwriteDatabaseId,
                 Config.appwriteCollectionId,
                 slug
             )
-            return true;
         } catch (error) {
             console.log("Appwrite service :: getPost :: error :: ",error.message);
             return false;
@@ -120,10 +119,10 @@ class Service {
         }
     }
 
-    getFilePreview(fileId){
+   async getFilePreview(fileId){
         return this.bucket.getFilePreview(
+            fileId,
             Config.appwriteBucketId,
-            fileId
         )
     }
 }
