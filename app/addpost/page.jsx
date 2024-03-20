4284ffc1d3ed967f8c010044378fcaff5997927d
Codearick <rohan.gautam650@gmail.com>
2024-03-21 03:06:16 +0530

    update

---
+++ app/addpost/page.jsx
@@ -0,0 +1,13 @@
+import {Container, PostForm} from '@/components/index'
+
+import React from 'react'
+
+const page = () => {
+  return (
+    <Container>
+        <PostForm/>
+    </Container>
+  )
+}
+
+export default page
