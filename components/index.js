efd2cd45b0959ae05e806130989eb91438712c49
Codearick <rohan.gautam650@gmail.com>
2024-03-22 03:50:18 +0530

    update

--- components/index.js
+++ components/index.js
@@ -1,14 +1,19 @@
 "use client"
-import Header from "./header/page";
-import Footer from "./footer/page"
+
+import Header from "./header/Header";
+import Footer from "./footer/Footer"
 import Logo from "./Logo"
-import Container from "./Container/Container"
+import Container from "./container/Container"
 import LogoutBtn from "./header/LogoutBtn"
 import PostCard from "./PostCard"
 import Input from "./input"
 import Select from "./Select"
 import Button from "./Button"
 import RTE from './RTE'
+import Signup from "./Signup";
+import Login from "./Login";
+import PostForm from './post-form/PostForm'
+import AuthLayout from './AuthLayout'
 
 export {
     Header,
@@ -20,5 +25,9 @@ export {
     PostCard,
     Select,
     Button,
-    RTE
+    RTE,
+    Signup,
+    Login,
+    PostForm,
+    AuthLayout,
 }
