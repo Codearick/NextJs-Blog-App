64c70ea82fd518ca26aada8b4c5cfd5df2ed4a7c
Codearick <rohan.gautam650@gmail.com>
2024-03-24 04:30:25 +0530

    update

--- app/page.js
+++ app/page.js
@@ -1,36 +1,53 @@
-"use client"
-// import Config from "@/app/config/config";
-import { useEffect,useState } from "react";
-import { useDispatch } from "react-redux";
-import authService from "./appwrite/auth";
-import {login, logout} from './store/slices/authSlice'
+'use client'
 
+import React, { useEffect, useState } from 'react'
+import appWriteService from '@/app/appwrite/appwriteConfig'
+import { Container, PostCard } from "@/components/index"
+import Link from 'next/link'
 
-export default function Home() {
-  const [loading, setLoading] = useState(true);
-  const dispatch = useDispatch();
+const page = () => {
+    const [posts, setPosts] = useState([]);
 
-  useEffect(() => {
-    authService.getCurrentUser()
-    .then((userData)=>{
-      if(userData){
-        dispatch(login({userData}))
-      } else{
-        dispatch(logout())
-      }
-    })
-    .catch(error =>{console.log(error.messsage)})
-    .finally(() => setLoading(false))
-  }, [])
-  
-  return !loading ? (
-    <div className='min-h-[80vh] flex flex-wrap content-between bg-gray-400'>
-      <div className='w-full block'>
-        <main>
-         TODO: 
-        </main>
-      </div>
-    </div>
-  ) : null
+    useEffect(() => {
+        appWriteService.getPosts().then(posts => {
+            if (posts) {
+                setPosts(posts.documents);
+            }
+        })
+    }, [])
+
+    if (posts.length === 0) {
+        return (
+            <div className="w-full min-h-[90vh] py-8 mt-4 text-center">
+                <Container>
+                    <div className="flex items-center justify-center my-auto">
+                        <div className="p-2 w-full">
+                            <h1 className="text-2xl font-bold hover:text-gray-500">
+                                Login to read posts
+                            </h1>
+                            <Link href={"/login"}>
+                                <p className='text-blue-500 hover:underline'>click here to login</p>
+                            </Link>
+                        </div>
+                    </div>
+                </Container>
+            </div>
+        )
+    }
+
+    return (
+        <div className='w-full py-8'>
+            <Container>
+                <div className='flex flex-wrap'>
+                    { posts.map(post => (
+                            <div key={posts.$id} className='p-2 w-1/4'>
+                                <PostCard {...post} />
+                            </div>
+                        ))}
+                </div>
+            </Container>
+        </div>
+    )
 }
 
+export default page
