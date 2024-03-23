ce317b791942ed148e16448be756b59b4fe8baa9
Codearick <rohan.gautam650@gmail.com>
2024-03-24 04:31:07 +0530

    Update

--- components/Logo.js
+++ components/Logo.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Logo = ({width = '200px'}) => {
   return (
-    <div className="text-slate-600 font-extrabold text-3xl">Next-Blog</div>
+    <div className="w-full text-slate-300 font-extrabold text-3xl">Next-Blog</div>
   )
 }
 
