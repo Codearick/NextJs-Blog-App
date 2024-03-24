ec5b72d485fdb68f74a95d7041ff3acbe5107a0a
Codearick <rohan.gautam650@gmail.com>
2024-03-25 04:46:51 +0530

    update

--- app/page.js
+++ app/page.js
@@ -39,11 +39,11 @@ const page = () => {
         <div className='w-full py-8'>
             <Container>
                 <div className='flex flex-wrap'>
-                    { posts.map(post => (
-                            <div key={posts.$id} className='p-2 w-1/4'>
-                                <PostCard {...post} />
-                            </div>
-                        ))}
+                    {posts.map(post => (
+                        <div key={post.$id} className='p-2 w-1/4'>
+                            <PostCard {...post} />
+                        </div>
+                    ))}
                 </div>
             </Container>
         </div>
