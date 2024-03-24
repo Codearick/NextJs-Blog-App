60dc1d22c5248464de22354bf70190a19142ca91
Codearick <rohan.gautam650@gmail.com>
2024-03-25 04:45:27 +0530

    Update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'https://cloud.appwrite.io/console/project-65ef35df017ae425b96d/storage/bucket-65ef3b034100cda1b12b',
+                port: '',
+            },
+        ],
+    },
+};
 
 export default nextConfig;
