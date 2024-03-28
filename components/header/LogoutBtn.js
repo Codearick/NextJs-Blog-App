7ba68834933ca5cb2e0f35ae454504254080f670
Codearick <rohan.gautam650@gmail.com>
2024-03-29 05:24:05 +0530

    update

--- components/header/LogoutBtn.js
+++ components/header/LogoutBtn.js
@@ -2,16 +2,19 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import { logout } from '@/app/store/slices/authSlice'
 import authService from '@/app/appwrite/auth'
-import store from '@/app/store/store'
+import {useRouter} from 'next/navigation'
 
 
 const LogoutBtn = () => {
     const dispatch = useDispatch();
+    const router = useRouter();
 
     const logoutHandler = async () => {
         try {
             await authService.logout();
             dispatch(logout());
+            router.push("/");
+
         } catch (error) {
             console.log("Error logging out: ", error.message)
         }
@@ -20,7 +23,7 @@ const LogoutBtn = () => {
     return (
         <div>
             <button 
-            className='inline-block px-6 py-2 duration-200 hover:bg-blue-300 rounded-2xl'
+            className='inline-block px-6 py-2 duration-200 hover:bg-red-500 rounded-2xl'
             onClick={logoutHandler}
             >Logout</button>
         </div>
