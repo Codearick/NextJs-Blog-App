81304e2aedd6ade3df0fac5d0290c2463892e978
Codearick <rohan.gautam650@gmail.com>
2024-05-19 06:20:12 +0530

    add bug fixes in the home page

--- app/page.js
+++ app/page.js
@@ -1,28 +1,27 @@
 'use client'
 
-import React, { useEffect, useState } from 'react'
-import appWriteService from '@/app/appwrite/appwriteConfig'
-import authService from './appwrite/auth'
-import { Container, PostCard } from "@/components/index"
-import Link from 'next/link'
+import React, { useEffect, useState } from 'react';
+import appWriteService from '@/app/appwrite/appwriteConfig';
+import { Container, PostCard } from "@/components/index";
+import Link from 'next/link';
+import { useSelector } from 'react-redux';
 
-const page = () => {
+const Page = () => {
     const [posts, setPosts] = useState([]);
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const authStatus = useSelector(state => state.auth.status);
 
     useEffect(() => {
-        appWriteService.getPosts().then(posts => {
-            if (posts) {
-                setPosts(posts.documents);
-            }
-        })
-        authService.getCurrentUser().then(user => {
-            if(user)setIsLoggedIn(true)
-        })
-    }, [])
+        appWriteService.getPosts([])
+            .then(posts => {
+                if (posts) {
+                    setPosts(posts.documents);
+                }
+            })
+            .catch(error => console.error("Failed to get all posts from Appwrite", error));
+    }, []);
 
-
-    if (isLoggedIn === false) {
+    if (posts.length === 0 || authStatus === false) {
         return (
             <div className="w-full min-h-[90vh] py-8 mt-4 text-center">
                 <Container>
@@ -31,29 +30,38 @@ const page = () => {
                             <h1 className="text-2xl font-bold hover:text-gray-500">
                                 Login to read posts
                             </h1>
-                            <Link href={"/login"}>
-                                <p className='text-blue-500 hover:underline'>click here to login</p>
+                            <Link href="/login">
+                                <p className="text-blue-500 hover:underline">Click here to login</p>
                             </Link>
                         </div>
                     </div>
                 </Container>
             </div>
-        )
+        );
     }
 
+    if (posts.length === 0 && authStatus) {
+        return (
+            <div className='h-[70vh] flex justify-center items-center'>
+                <Container />
+            </div>
+        )
+    }
+console.log("POSTS ARE ::", posts)
     return (
-        <div className='w-full min-h-[90vh] py-8'>
+        <div className="w-full min-h-[90vh] py-8">
             <Container>
                 <div className='flex flex-wrap'>
-                    {posts.map(post => (
+                    {posts.length == 0 && <div className='text-xl text-white'>Oops! No Posts to show!</div>}
+                    {posts.map((post) => (
                         <div key={post.$id} className='p-2 w-1/4'>
-                            <PostCard {...post} />
+                            <PostCard post={post} />
                         </div>
                     ))}
                 </div>
             </Container>
         </div>
-    )
-}
+    );
+};
 
-export default page
+export default Page;
