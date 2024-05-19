5326fa4f02f38535c28f18ce771469ef06c30b44
Codearick <rohan.gautam650@gmail.com>
2024-05-20 01:41:01 +0530

    add postcard bugfixes

--- components/PostCard.js
+++ components/PostCard.js
@@ -4,23 +4,31 @@ import Link from 'next/link'
 import Image from 'next/image'
 
 
-const PostCard = ({ $id, title, featuredImage }) => {
-    const img = Service.getFilePreview(featuredImage);
-    const link = `/blog/${$id}`
+const PostCard = ({ post }) => {
+    const img = Service.getFilePreview(post.featuredImage);
+    const link = `/blog/${post.$id}`
+    const date = new Date(post.$createdAt).toLocaleDateString('en-US');
     return (
-        <Link >
-            <div className='w-full bg-gray-100 rounded-lg p-4'>
-                <div className="relative w-full h-auto justify-center mb-4">
-                <Image
-                    className='rounded-lg'
-                    fill={true}
-                    objectFit='contain'
-                    src={img}
-                    alt={title}
-                    priority
-                />
+        <Link href={link}>
+            <div className='w-full bg-gray-100 text-black p-4 max-w-md shadow-xl rounded-xl overflow-hidden h-full'>
+                <div className="relative w-full h-64 mb-4">
+                    <Image
+                        className='rounded-lg'
+                        src={img}
+                        alt={post.title}
+                        layout='fill'
+                        objectFit='contain'
+                        priority
+                    />
                 </div>
-                <h2 className='font-bold text-xl text-black'>{title}</h2>
+
+                <div className="sm:px-6 px-2 py-4 flex flex-wrap justify-between items-center">
+                    <div className="font-bold text-xl mb-2">{post.title}</div>
+                    <p className="text-sm">{date}</p>
+                </div>
+
+
+                <h2 className='font-bold text-xl text-black'>{post.title}</h2>
             </div>
         </Link>
     )
