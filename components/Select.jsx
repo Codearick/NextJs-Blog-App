3b895f9031f03c21537c1dcffee7c3a894f431f6
Codearick <rohan.gautam650@gmail.com>
2024-05-20 03:48:34 +0530

    add the bugfix in the select tag component

--- components/Select.jsx
+++ components/Select.jsx
@@ -16,7 +16,7 @@ const Select = ({
                 className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
             >
                 {options && options.map(option => (
-                    <option key={id} value={option}>
+                    <option key={option} value={option}>
                         {option}
                     </option>
                 ))}
