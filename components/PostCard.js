cb074b66bfccac8f7f1b5fa4fe8dc3c1ffae8172
Codearick <rohan.gautam650@gmail.com>
2024-05-27 05:42:40 +0530

    add changes in the postcard

--- components/PostCard.js
+++ components/PostCard.js
@@ -6,29 +6,27 @@ import Image from 'next/image'
 
 const PostCard = ({ post }) => {
     const img = appwriteService.getFilePreview(post.featuredImage);
-    console.log("THIS IS IMG :: ", img.href)
     const link = `/blog/${post.$id}`
     const date = new Date(post.$createdAt).toLocaleDateString('en-US');
     return (
-        <Link href={link}>
-            <div className='w-full bg-gray-100 text-black p-4 max-w-md shadow-xl rounded-xl overflow-hidden h-full'>
-                <div className="relative w-full h-64 mb-4">
-                    <Image
-                        className='rounded-lg'
-                        src={`${img}`}
-                        alt={`${post.title}`}
-                        width={700}
-                        height={700}
-                    />
-                </div>
+        <Link href={`${link}`}>
+            <div className="max-w-md border border-[rgb(139,137,139)] px-5 py-7 shadow-xl rounded-xl overflow-hidden h-full">
+                <Image 
+                    className="w-full h-auto" 
+                    src={img.href} 
+                    alt={post.title} 
+                    width={800}
+                    height={500}
+                />
 
                 <div className="sm:px-6 px-2 py-4 flex flex-wrap justify-between items-center">
                     <div className="font-bold text-xl mb-2">{post.title}</div>
-                    <p className="text-sm">{date}</p>
+                    <div className="mt-2 text-sm">{date}</div>
                 </div>
 
-
-                <h2 className='font-bold text-xl text-black'>{post.title}</h2>
+                <div className="px-6 py-4 flex justify-between items-center">
+                    <div className="text-sm">{post.status}</div>
+                </div>
             </div>
         </Link>
     )
